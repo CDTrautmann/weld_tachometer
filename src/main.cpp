9a7f14778e1cbd5c3f64f51a2fc7341f313c41a3
CDTrautmann <cdtrautm@ualberta.ca>
2023-02-15 13:10:14 -0700

    fixed presicion on the displayed energy

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 //TODO: measure and update the dimensions
 // test on the LCD library
 const int tachPin = 0;
-float value=0;
+double rounded=0;
 int tach=0;
-float sfm=0;
-float rpm = 0;
+double sfm=0;
+double rpm = 0;
 int oldtime=0;
 int time;
-const int holdRad = 457;
+const int holdDia = 457;
 const int tachWidth = 5;
-const int wheelRad = 914;
+const int wheelDia = 914;
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -23,13 +23,14 @@ void isr() {
 }
 
 void writeSpeed(float rpm) {
-  sfm = rpm*(wheelRad*6.2832)*(1/1000);
+  sfm = rpm*(wheelDia*6.2832)*(1/1000);
+  rounded = round(sfm*100)/100;
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print('_ROTATION SPEED_');
+  lcd.print("_SURFACE SPEED_");
   lcd.setCursor(0,1);
-  lcd.print(sfm);
-  lcd.print('  M/min');
+  lcd.print(rounded);
+  lcd.print(" M/min");
 }
 
 void setup() {
@@ -44,7 +45,7 @@ void loop() {
   delay(500);
   detachInterrupt(tachPin);
   time = millis()-oldtime;
-  rpm = (tach/time)*(1/tachWidth)*(1/(holdRad*6.2832))*60000;
+  rpm = (tach/time)*(1/tachWidth)*(1/(holdDia*6.2832))*60000;
   oldtime = millis();
   tach = 0;
   writeSpeed(rpm);
