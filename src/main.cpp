cb479921aec426df29f5e16e4780db48636629f3
CDTrautmann <cdtrautm@ualberta.ca>
2023-02-17 09:58:57 -0700

    increasing tachmoter resolution and inter debounce

--- src/main.cpp
+++ src/main.cpp
@@ -5,52 +5,58 @@
 //TODO: measure and update the dimensions
 // test on the LCD library
 const int tachPin = 2;
-double rounded=0;
-int tach=0;
+int unsigned tach=0;
 double sfm=0;
 double rpm = 0;
-int oldtime=0;
-int time =0;
-const int holdDia = 457;
-const int tachWidth = 6;
-const int wheelDia = 914;
+int unsigned long oldtime=0;
+int unsigned long time =0;
+const unsigned int holdDia = 457;
+const unsigned int tachWidth = 17;
+const unsigned int wheelDia = 914;
+
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void isr() {
-  tach++;
+  static unsigned long lastInterruptTime = 0;
+  unsigned long interruptTime = millis();
+  if (interruptTime - lastInterruptTime > 100){
+    tach++;
+  }
+  lastInterruptTime = interruptTime;
 }
 
 void writeSpeed(float rpm) {
   sfm = (rpm*(wheelDia*6.2832)/1000);
-  rounded = round(sfm*100)/100;
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("_SURFACE SPEED_");
   lcd.setCursor(0,1);
-  lcd.print(rounded);
+  lcd.print(sfm);
   lcd.print(" M/min");
-  Serial.println(rounded);
+  Serial.print("M/min: ");
+  Serial.println(sfm);
 }
 
 void setup() {
   digitalWrite(2, HIGH);
   attachInterrupt(digitalPinToInterrupt(tachPin),isr,RISING);
-  attachInterrupt(digitalPinToInterrupt(tachPin),isr,FALLING);
   lcd.begin(16, 2);
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(2000);
   detachInterrupt(tachPin);
   time = millis()-oldtime;
+  Serial.print("tach: ");
+  Serial.print(tach);
   rpm = (((tach*60000/time)*tachWidth)/(holdDia*3.1415));
+  Serial.print("  RPM: ");
   Serial.println(rpm);
   oldtime = millis();
   tach = 0;
   writeSpeed(rpm);
   attachInterrupt(digitalPinToInterrupt(tachPin),isr,RISING);
-  attachInterrupt(digitalPinToInterrupt(tachPin),isr,FALLING);
+  delay(10000);
 }
